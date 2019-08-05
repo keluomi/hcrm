729dbdb511ba7baff918b47c5e0737e0acdf70e9
lwz <459671458@qq.com>
2019-08-05 11:44:29 +0800

    测试

--- components/fui-grid/fui-grid.vue
+++ components/fui-grid/fui-grid.vue
@@ -2,25 +2,33 @@
 	<view>
 		<view class="cu-list grid" :class="['col-' + gridCol,gridBorder?'':'no-border']">
 			<view class="cu-item" v-for="(item,index) in gridData" :key="index" v-if="index<gridCol*2">
-				<!-- <view :class="[gridMode =='col'?'grid':'',gridMode =='col'?'col-2':'']"> -->
-				<view class="flex flex-wrap">
-					<view class="basis-df  margin-xs">
+				<view v-if="gridMode !='col'">
+					<view>
+						<view :class="['cuIcon-' + item.cuIcon,'text-' + item.color]">
+							<view class="cu-tag badge" v-if="item.badge && item.badge!=0">
+								<block v-if="item.badge!=1">{{item.badge>99?'99+':item.badge}}</block>
+							</view>
+						</view>
+					</view>
+					<view>
+						<view><text>{{item.name}}</text></view>
+					</view>
+					
+				</view>
+				<view v-if="gridMode == 'col'" class="flex">
+					<view class="basis-xs">
 						<view :class="['cuIcon-' + item.cuIcon,'text-' + item.color,'radius']">
 							<view class="cu-tag badge" v-if="item.badge && item.badge!=0">
 								<block v-if="item.badge!=1">{{item.badge>99?'99+':item.badge}}</block>
 							</view>
 						</view>
-			<!-- 			<view>
-							<view><text>{{item.name}}</text></view>
-							<view v-if="gridMode"><text>{{item.subName}}</text></view>
-						</view> -->
 					</view>
-					<view class="basis-df margin-xs">
+					<view class="basis-sm">
 						<view><text>{{item.name}}</text></view>
-						<view v-if="gridMode"><text>{{item.subName}}</text></view>
+						<view><text>{{item.subName}}</text></view>
 					</view>
-
 				</view>
+
 
 			</view>
 			
