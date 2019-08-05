95772e1e846fccdee04585cc54f79edc696cfd53
lwz <459671458@qq.com>
2019-08-05 15:20:52 +0800

    修改list组件

--- components/fui-list/fui-list.vue
+++ components/fui-list/fui-list.vue
@@ -1,12 +1,14 @@
 <template>
 	<view class="cu-list menu" :class="[isBorder?'sm-border':'',isCard?'card-menu margin-top':'']">
 		<view class="cu-item" v-for="(item,index) in listData" :key="index" :class="isArrow?'arrow':''">
-			<navigator class="content" hover-class="none" :url="item.url" :open-type="item.openType?item.openType:'navigate'">
-				<text :class="['cuIcon','cuIcon-'+item.icon, item.color?'text-'+item.color:'text-'+'jianniaolan']"></text>
-				<text class="text-grey">{{item.text}}</text>
+			<navigator class="content flex" hover-class="none" :url="item.url" :open-type="item.openType?item.openType:'navigate'">
+				<text v-if="item.icon" :class="['cuIcon','cuIcon-'+item.icon, item.color?'text-'+item.color:'text-'+'jianniaolan']"></text>
+				<text class="flex-twice">{{item.text}}</text>
+				<text class="flex-sub"  v-if="item.subText" >{{item.subText}}</text>
 			</navigator>
 		</view>
 	</view>
+
 </template>
 
 <script>
